42d336c4d12fbc7e88ac937b8a48fcdbf4d4c965
s0lenas <s0len@DESKTOP-IGSIS6N.>
2024-12-08 00:32:03 +0200

    Added Terraform configuration file for EC2

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_instance" "resume-server" {
+    ami = "ami-075449515af5df0d1"
+    instance_type = "t3.micro"
+    user_data = <<EOF
+#!/bin/bash
+echo "Copying the SSH Key to the server"
+echo -e "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDY3Sb7faJaWIbThlXI5l7vMtjEz/hvgTJRBXp7YB7fl4EOfANDyxbfgf9FNbPI3kRWqLAjfNhIqtytuhoDQsEeacUNU6sMVqUupYUACuTOcYh/8ALb8WSiwKy2YEDzjnMoaFdIqI+k0Vw9ObR4Ow3RILUX2gm5y3Ll0XVVpbi1Xb6nNZOTANDTH+mQL7SvtmVuysTO+Zahv2ZzDhfvzx8p+cWXShhwbvgIWNg4aRZ9uq54zcwOKDvHiuqS9aZZmeYp5RgMlGrc6Zp4jSvSkKQiITeN92dtBeGY67D68wb2EQqjCQxB7zPgdqFNIEcIBnwbMY+thVhS0qYhlx7rInIQWw6F6iALlc1A9fuIhk7oFHDDozjoGQM8mej5ET3nkhKA7zoed3khVsskXCiRWUtg8Ijj/G+LFKF5EL1qRIDF4xiEVu9k9InfIP72gxNCjjilB6AbOL7Yo9kK6/cMRecqqclNXnF1DY6Tn+EP8DsXFuf4/occGTccF/H5UBAeHU57ec22Ko6/SZfs/8HKfH4m3z9qQ8tpmdH3Gx6K8n7MgfaPlzAW0hmGA5Bkh5AaLCDb9Z+o2yPn3MsIIH9/gBBHORiQyAVuuT69eMrtnToEpuqV2iS4UTaPnyvMm3W9MmFyi3hZpV/FbPke8Qq4Wz+hPjpnTIiMc2VwAJN1XOrF3w== s0len@DESKTOP-IGSIS6N"
+EOF
+
+
+    tags = {
+        Name = "ResumeServer"
+    }
+}
